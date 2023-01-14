05077326a0884aecb2580eb71aad6c87679deb89
[Nevin Mathews] <[nevinmathewsk@gmail.com]>
2023-01-15 00:35:43 +0530

    Created GraphQL Mutations

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 require('dotenv').config();
 
 const connectDB=async()=>{
-    const conn= await mongoose.connect('mongodb+srv://KetTroyDsz:yuhhh@splyt.meqgytv.mongodb.net/ProjectManagement?retryWrites=true&w=majority');
+    const conn= await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected`);
 }
 
