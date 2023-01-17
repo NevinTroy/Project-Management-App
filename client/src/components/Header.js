cbc93489226e1056b80f2a3597cf8bda100fdf38
[Nevin Mathews] <[nevinmathewsk@gmail.com]>
2023-01-18 01:32:00 +0530

    Final Changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
     <nav className='navbar bg-light mb-4 p-0'>
             <a className='navbar-brand'>
                 <div className='d-flex'>
-                    {/* <img src={project} alt='logo' classNameName='h-10 w-25'/> */}
-                    <div>Project Management</div>
+                    <img src={project} alt='logo' classNameName='h-10 w-25'/>
+                    <div className='mt-3 '><strong>Project Management</strong></div>
                 </div>  
             </a>
     </nav>
